e2ef8c403fa5f3c44a24d25d26a8024466820803
MATHEUS STEFANI DOS SANTOS <41953@eep.br>
2024-08-31 00:26:26 -0300

    Add project files.

---
+++ SomaTargetSistemas/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SomaTargetSistemas
+{
+    using static Console;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 13;
+            int SOMA = 0;
+            int K = 0;
+
+            while(K < INDICE)
+            {
+              //K = K + 1 OU
+                K += 1;
+              //SOMA = SOMA + K OU
+                SOMA += K;
+            }
+            Write(SOMA);
+
+        }
+
+    }
+}
